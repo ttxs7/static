3c2517ac2f9569dcdfff63eef32178678698977c
ttxs <ttxs@pku.edu.cn>
2020-10-04 15:15:21 +0800

    add js and st

---
+++ js/test-parsers.js
@@ -0,0 +1,7 @@
+return {
+    myUnit:(unit,config,extUnitParsers)=>{
+        const element=document.createElement('span')
+        element.textContent='Hi '+unit.options.object.name
+        return element
+    }
+}
