7b76e47b31e756105cf9443ad9da4078db52ccec
ttxs <ttxs@pku.edu.cn>
2020-10-11 14:48:38 +0800

    modify js

--- js/test-parsers.js
+++ js/test-parsers.js
@@ -1,7 +1,5 @@
-export const myUnits={
-    myUnit:(unit,config,extUnitParsers)=>{
-        const element=document.createElement('span')
-        element.textContent='Hi '+unit.options.object.name
-        return element
-    }
+export function myUnit(unit,config,extUnitParsers){
+    const element=document.createElement('span')
+    element.textContent='Hi '+unit.options.object.name
+    return element
 }
